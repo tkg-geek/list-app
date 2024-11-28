04d8ef4d453d4d092db44355e8f29cb4008e9428
tkg-geek <tkg.geek@gmail.com>
2024-11-28 13:03:07 +0900

    dump deleted

--- db.php
+++ db.php
@@ -8,8 +8,8 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 // 環境変数が正しく読み込まれているか確認
-var_dump($_ENV);
-exit;
+// var_dump($_ENV);
+// exit;
 
 $host = $_ENV['DB_HOST'] ?? 'localhost';
 $dbname = $_ENV['DB_NAME'] ?? 'list_app';
