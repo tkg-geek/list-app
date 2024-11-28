604dd9b2e5c9adbf6d054bb4b5aa6f148c67463f
tkg-geek <tkg.geek@gmail.com>
2024-11-28 15:15:30 +0900

    test

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
                 PHPに書き換えたシンプルなリストツールです。<br>
                 甘く見ていたら予想以上に苦労しました。<br>
                 なお、今回のmissionはGitです。<br>
-                mainブランチをGitHubにpushすればさくらサーバーも自動デプロイされます。<br>
+                自動デプロイテスト<br>
             </p>
             <a class="index-button" href="list.php">一覧を見る</a>
         </div>
